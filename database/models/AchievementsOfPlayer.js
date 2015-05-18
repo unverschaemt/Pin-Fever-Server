0f5e060b254acf808a21c2b4a0fc7d89aac4188c
dustin-H <dustin.hoffner@gmail.com>
2015-05-18 13:38:28 +0200

    Correct nTOm references

--- database/models/AchievementsOfPlayer.js
+++ database/models/AchievementsOfPlayer.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose');
+var Player = require('./Player.js');
+var Achievement = require('./Achievement.js');
 
 var Schema = mongoose.Schema;
 
 var fileSchema = new Schema({
-    playerID: String,
-    achievementID: String
+    playerID: {type: Schema.ObjectId, ref: 'Player'},
+    achievementID: {type: Schema.ObjectId, ref: 'Achievement'}
 });
 
 module.exports = mongoose.model('AchievementsOfPlayer', fileSchema);
