c587645642196e3d40dab144706b6d71762eafc4
dustin-H <dustin.hoffner@gmail.com>
2015-05-27 17:09:03 +0200

    Fixed bug with get post redirect

--- apis/Players.js
+++ apis/Players.js
@@ -2,6 +2,9 @@ var express = require('express');
 var playerIdMe = require('../utils/playerIdMe.js');
 var Player = require('../database/models/Player.js');
 
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
 module.exports = function() {
 	var app = express();
 
@@ -39,12 +42,27 @@ module.exports = function() {
 		}
 	});
 
-	app.post('/players/me/set', function(req, res) {
-		if (req.params != null && req.query != null && req.params.collection != null) {
+	app.post('/players/me/set', jsonParser, function(req, res) {
+		if (req.body != null) {
 			var language = req.query.language || 'EN';
-			var maxResults = req.query.maxResults || 10;
-			var pageToken = req.query.pageToken || '';
-			res.internalError('Not implemented!', 'Not implemented!');
+			Player.findOne({
+				_id: req.auth._id
+			}, function(err, player) {
+				if (err || !player) {
+					return res.internalError('Database error or user not found!');
+				}
+				for (var i in req.body) {
+					if (['avatarImageUrl', 'displayName'].indexOf(i) >= 0) {
+						player[i] = req.body[i];
+					}
+				}
+				player.save(function(err, newplayer) {
+					if (err) return res.internalError(err);
+					res.success({
+						player: newplayer
+					});
+				});
+			}).select('avatarImageUrl displayName email level _id');
 		}
 	});
 
--- auth/index.js
+++ auth/index.js
@@ -1,19 +1,43 @@
 var express = require('express');
 var jwt = require('jsonwebtoken');
 var passwordHash = require('password-hash');
-var bodyParser = require('body-parser')
 
-var jsonParser = bodyParser.json()
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 
 var Player = require('../database/models/Player.js');
 
 var secret = 'ThisIsAnotSecretKeyWhichShouldGetReplaced';
 
+var loginUser = function(email, password, res) {
+	Player.findOne({
+		email: email
+	}, function(err, player) {
+		if (err || !player) {
+			res.paramError(err, 'User not found!');
+		} else {
+			if (passwordHash.verify(password, player.password)) {
+				var token = jwt.sign({
+					email: player.email,
+					displayName: player.displayName,
+					avatarURL: player.avatarURL,
+					_id: player._id,
+					login: true
+				}, secret);
+				res.success({
+					token: token
+				});
+			} else {
+				res.paramError(err, 'Invalid password!');
+			}
+		}
+	});
+}
+
 module.exports = function() {
 	var app = express();
 
 	app.use(function(req, res, next) {
-		console.log('headers',req.headers);
 		if (req.headers != null && req.headers['api-auth-token'] != null && typeof req.headers['api-auth-token'] === 'string' && req.headers['api-auth-token'].length > 0) {
 			jwt.verify(req.headers['api-auth-token'], secret, function(err, decoded) {
 				if (err == null) {
@@ -28,36 +52,14 @@ module.exports = function() {
 
 	app.post('/auth/login', jsonParser, function(req, res, next) {
 		if (req.body != null && req.body.email != null && req.body.password != null) {
-
-			Player.findOne({
-				email: req.body.email
-			}, function(err, player) {
-				if (err || !player) {
-					res.paramError(err, 'User not found!');
-				} else {
-					if(passwordHash.verify(req.body.password, player.password)){
-						var token = jwt.sign({
-							email: player.email,
-							displayName: player.displayName,
-							avatarURL: player.avatarURL,
-							_id: player._id,
-							login: true
-						}, secret);
-						res.success({
-							token: token
-						});
-					} else {
-						res.paramError(err, 'Invalid password!');
-					}
-				}
-			});
+			loginUser(req.body.email, req.body.password, res);
 		} else {
 			res.paramError('invalid params');
 		}
 	});
 
 	app.post('/auth/register', jsonParser, function(req, res, next) {
-		if(req.body != null && req.body.email != null && req.body.displayName && req.body.password){
+		if (req.body != null && req.body.email != null && req.body.displayName && req.body.password) {
 			Player.findOne({
 				email: req.body.email
 			}, function(err, player) {
@@ -70,14 +72,14 @@ module.exports = function() {
 					});
 					newplayer.save(function(err, newplayer) {
 						if (err) return res.internalError(err);
-						res.redirect('/auth/login?email='+req.body.email+'&password='+req.body.password)
+						loginUser(req.body.email, req.body.password, res);
 					});
 				} else {
 					res.paramError('Email already registered!', 'Email already registered!');
 				}
 			});
 		} else {
-			res.paramError('invalid params','no param');
+			res.paramError('invalid params', 'no param');
 		}
 	});
 
