3b252fb6d2668e4bf6ce64957b5a5720b6a09e0d
dustin-H <dustin.hoffner@gmail.com>
2015-05-30 16:47:25 +0200

    Middleware for loading authenticated player data

---
+++ utils/loadAuthPlayer.js
@@ -0,0 +1,26 @@
+var express = require('express');
+
+module.exports = function() {
+	var app = express();
+
+	app.use(function(req, res, next) {
+		if (req.auth != null && req.auth._id != null) {
+			Player.findOne({
+				_id: req.auth._id
+			}, function(err, player) {
+				if (err) {
+					return res.internalError('Database error or user not found!');
+				}
+				if(!player){
+					res.unauthorizedError('Login needed for this section!');
+				}
+				req.player = player;
+				next();
+			}).select('displayName level _id email');
+		} else {
+			res.unauthorizedError('Login needed for this section!');
+		}
+	});
+
+	return app;
+};
