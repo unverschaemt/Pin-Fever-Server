a49a8458999228051eb7ac537e8bcd49a0831354
dustin-H <dustin.hoffner@gmail.com>
2015-06-02 13:54:42 +0200

    random categories and random questions updated

--- apis/Questions.js
+++ apis/Questions.js
@@ -13,6 +13,119 @@ var jsonParser = bodyParser.json();
 
 var querySelects = require('../config/querySelects.js');
 
+var randomcategories = function(req, res) {
+	var query = Category.findRandom();
+	query.limit(req.query.amount);
+	query.select('_id name');
+	query.exec(function(err, categories) {
+		if (err) {
+			return res.internalError('Database error!');
+		}
+		var languageQuery = Text.find();
+
+		var orquery = [];
+		for (var i in categories) {
+			orquery.push({
+				language: req.query.language,
+				_translation: mongoose.Types.ObjectId(categories[i].name)
+			});
+		}
+
+		languageQuery.or(orquery);
+
+		languageQuery.exec(function(err, texts) {
+			if (err) {
+				return res.internalError('Database error!');
+			}
+			if (texts.length < orquery.length && req.query.language === 'en') {
+				return res.internalError('Language text not found!', 'Language text not found!');
+			}
+			if (texts.length < orquery.length) {
+				req.query.language = 'en';
+				return randomcategories(req, res);
+			}
+			var textsObj = {};
+			for (var i in texts) {
+				textsObj[texts[i]._translation.toString()] = texts[i].content;
+			}
+			var outCategories = [];
+			for (var i in categories) {
+				var outCategory = {};
+				outCategory._id = categories[i]._id;
+				var oName = textsObj[categories[i].name.toString()];
+				console.log('oName', oName);
+				outCategory.name = oName;
+				outCategories.push(outCategory);
+			}
+			res.success({
+				categories: outCategories
+			});
+		});
+	});
+};
+
+var randomquestions = function(req, res) {
+	var query = Question.findRandom();
+	if (req.query.category) {
+		query.where({
+			category: mongoose.Types.ObjectId(req.query.category)
+		});
+	}
+	query.limit(req.query.amount);
+	query.exec(function(err, questions) {
+		if (err) {
+			return res.internalError('Database error!');
+		}
+		var languageQuery = Text.find();
+		//languageQuery.populate('language');
+		var orquery = [];
+		for (var i in questions) {
+			orquery.push({
+				language: req.query.language,
+				_translation: mongoose.Types.ObjectId(questions[i].question)
+			});
+			orquery.push({
+				language: req.query.language,
+				_translation: mongoose.Types.ObjectId(questions[i].answer.text)
+			});
+		}
+
+		languageQuery.or(orquery);
+		languageQuery.exec(function(err, texts) {
+			if (err) {
+				return res.internalError('Database error!');
+			}
+			if (texts.length < orquery.length && req.query.language === 'en') {
+				return res.internalError('Language text not found!', 'Language text not found!');
+			}
+			if (texts.length < orquery.length) {
+				req.query.language = 'en';
+				return randomquestions(req, res);
+			}
+			var textsObj = {};
+			for (var i in texts) {
+				textsObj[texts[i]._translation.toString()] = texts[i].content;
+			}
+			var outQuestions = [];
+			for (var i in questions) {
+				var outQuestion = {};
+				outQuestion._id = questions[i]._id;
+				outQuestion.category = questions[i].category;
+				outQuestion.answer = {};
+				outQuestion.answer.coordinates = questions[i].answer.coordinates;
+				var qtext = textsObj[questions[i].question.toString()];
+				var atext = textsObj[questions[i].answer.text.toString()];
+				outQuestion.question = qtext;
+				outQuestion.answer.text = atext;
+				outQuestions.push(outQuestion);
+			}
+			res.success({
+				questions: outQuestions
+			});
+		});
+	});
+};
+
 module.exports = function() {
 	var app = express();
 
@@ -31,16 +144,9 @@ module.exports = function() {
 
 	app.get('/randomcategories', function(req, res) {
 		req.query.amount = req.query.amount || 1;
-		var query = Category.findRandom();
-		query.limit(req.query.amount);
-		query.exec(function(err, categories) {
-			if (err) {
-				return res.internalError('Database error!');
-			}
-			res.success({
-				categories: categories
-			});
-		});
+		req.query.language = req.query.language || 'en';
+
+		randomcategories(req, res);
 	});
 
 	app.get('/random', function(req, res) {
@@ -48,58 +154,7 @@ module.exports = function() {
 		req.query.language = req.query.language || 'en';
 		req.query.category = req.query.category || false;
 
-		var query = Question.findRandom();
-		if (req.query.category) {
-			query.where({
-				category: mongoose.Types.ObjectId(req.query.category)
-			});
-		}
-		query.limit(req.query.amount);
-		query.exec(function(err, questions) {
-			if (err) {
-				return res.internalError('Database error!');
-			}
-			var languageQuery = Text.find();
-			//languageQuery.populate('language');
-			var orquery = [];
-			for (var i in questions) {
-				orquery.push({
-					language: req.query.language,
-					_translation: mongoose.Types.ObjectId(questions[i].question)
-				});
-				orquery.push({
-					language: req.query.language,
-					_translation: mongoose.Types.ObjectId(questions[i].answer.text)
-				});
-			}
-
-			languageQuery.or(orquery);
-			languageQuery.exec(function(err, texts) {
-				if (err) {
-					return res.internalError('Database error!');
-				}
-				var textsObj = {};
-				for (var i in texts) {
-					textsObj[texts[i]._translation.toString()] = texts[i].content;
-				}
-				var outQuestions = [];
-				for (var i in questions) {
-					var outQuestion = {};
-					outQuestion._id = questions[i]._id;
-					outQuestion.category = questions[i].category;
-					outQuestion.answer = {};
-					outQuestion.answer.coordinates = questions[i].answer.coordinates;
-					var qtext = textsObj[questions[i].question.toString()];
-					var atext = textsObj[questions[i].answer.text.toString()];
-					outQuestion.question = qtext;
-					outQuestion.answer.text = atext;
-					outQuestions.push(outQuestion);
-				}
-				res.success({
-					questions: outQuestions
-				});
-			});
-		});
+		randomquestions(req, res);
 	});
 
 	return app;
